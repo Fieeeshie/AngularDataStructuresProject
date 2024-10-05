ff5dca62ab76b65489c3336047e2a32e52051ce8
Fiercie <165393567+Fieeeshie@users.noreply.github.com>
2024-10-06 00:16:15 +0800

    Update booklist.component.ts

--- AngularDatastructuresProject/src/app/components/booklist/booklist.component.ts
+++ AngularDatastructuresProject/src/app/components/booklist/booklist.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './booklist.component.css'
 })
 export class BooklistComponent {
-  bookList: string[] = ['1984', 'To Kill a Mockingbird', 'The Great Gatsby', 'Pride and Prejudice', 'Moby-Dick'];
+  bookList: string[] = [
+    'Noli Me Tangere by José Rizal',
+    'El Filibusterismo by José Rizal',
+    'The Woman Who Had Two Navels by Nick Joaquin',
+    'Dogeaters by Jessica Hagedorn',
+    'Smaller and Smaller Circles by F.H. Batacan'
+  ];
   bookName: string = '';
 
   addBooks() {
