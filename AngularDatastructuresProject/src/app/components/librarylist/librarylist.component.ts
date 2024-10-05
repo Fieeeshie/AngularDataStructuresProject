f0ed69af4eb1cdc205f2f5a9ef3b8264546b8fb3
Fiercie <165393567+Fieeeshie@users.noreply.github.com>
2024-10-06 00:28:20 +0800

    Update librarylist.component.ts

--- AngularDatastructuresProject/src/app/components/librarylist/librarylist.component.ts
+++ AngularDatastructuresProject/src/app/components/librarylist/librarylist.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './librarylist.component.css'
 })
 export class LibrarylistComponent {
-  libraryList: string[] = [];
+  libraryList: string[] = [
+    'National Library of the Philippines',
+    'Ateneo de Manila University Library',
+    'University of the Philippines Main Library',
+    'De La Salle University Library',
+    'San Beda University Library'
+  ];
   libraryName: string = '';
 
   addLibraries() {
