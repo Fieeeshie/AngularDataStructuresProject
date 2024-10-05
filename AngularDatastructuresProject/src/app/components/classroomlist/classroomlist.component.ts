fccc02d103d37b77a4138003b17d8dd7731c7ae5
Fiercie <165393567+Fieeeshie@users.noreply.github.com>
2024-10-06 00:18:22 +0800

    Update classroomlist.component.ts

--- AngularDatastructuresProject/src/app/components/classroomlist/classroomlist.component.ts
+++ AngularDatastructuresProject/src/app/components/classroomlist/classroomlist.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './classroomlist.component.css'
 })
 export class ClassroomlistComponent {
-  classroomList: string[] = [];
+  classroomList: string[] = [
+    'Room 169 - Science Lab',
+    'Room 269 - Math Lab',
+    'Room 369 - Computer Lab',
+    'Room 469 - Art Room',
+    'Room 569 - Library'
+  ];
   classroomName: string = '';
 
   addClassrooms() {
