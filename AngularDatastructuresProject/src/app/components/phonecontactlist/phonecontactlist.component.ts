17bfac1b17430bf8cfbd8b881cbc99af0a049d5e
Fiercie <165393567+Fieeeshie@users.noreply.github.com>
2024-10-06 00:30:35 +0800

    Update phonecontactlist.component.ts

--- AngularDatastructuresProject/src/app/components/phonecontactlist/phonecontactlist.component.ts
+++ AngularDatastructuresProject/src/app/components/phonecontactlist/phonecontactlist.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './phonecontactlist.component.css'
 })
 export class PhonecontactlistComponent {
-  phoneContactList: string[] = [];
+  phoneContactList: string[] = [
+    'Walter John Wick - 123-456-7890',
+    'Princess Roerenz Tiozon - 987-654-3210',
+    'Rodney No Surname - 555-555-5555',
+    'Xavier The Tiktoker - 444-444-4444',
+  ];
   phoneContactName: string = '';
 
   addPhoneContacts() {
