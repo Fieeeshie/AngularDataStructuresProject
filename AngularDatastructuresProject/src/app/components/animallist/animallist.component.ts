64fec43081fbfa15fc5a3eca3f8a78e8250aae2b
Fiercie <165393567+Fieeeshie@users.noreply.github.com>
2024-10-05 23:28:25 +0800

    feat: add Animal List  component
    Animal List

---
+++ AngularDatastructuresProject/src/app/components/animallist/animallist.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animallist',
+  templateUrl: './animallist.component.html',
+  styleUrl: './animallist.component.css'
+})
+export class AnimallistComponent {
+  animalList: string[] = ['Lion', 'Elephant', 'Tiger', 'Giraffe', 'Zebra'];
+  animalName: string = '';
+
+  addAnimals() {
+    this.animalList.push(this.animalName);
+  }
+}
