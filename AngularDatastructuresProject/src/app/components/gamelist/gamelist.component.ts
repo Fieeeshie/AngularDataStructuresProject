c1d036fcd2d57fcf0159928439256b4094479788
Fieeeshie <toribiofiercie143@gmail.com>
2024-10-19 12:25:48 +0800

    Act-23

--- AngularDatastructuresProject/src/app/components/gamelist/gamelist.component.ts
+++ AngularDatastructuresProject/src/app/components/gamelist/gamelist.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-gamelist',
-  templateUrl: './gamelist.component.html',
-  styleUrl: './gamelist.component.css'
-})
-export class GamelistComponent {
-  gameList: string[] = [
-    'Mobile Legends: Bang Bang',
-    'Call of Duty: Mobile',
-    'Genshin Impact',
-    'Valorant',
-    'PUBG Mobile'
-  ];
-  gameName: string = '';
+  @Component({
+    selector: 'app-gamelist',
+    templateUrl: './gamelist.component.html',
+    styleUrl: './gamelist.component.css'
+  })
+  export class GamelistComponent {
+    gameList: string[] = [
+      'Mobile Legends: Bang Bang',
+      'Call of Duty: Mobile',
+      'Genshin Impact',
+      'Valorant',
+      'PUBG Mobile'
+    ];
+    gameName: string = '';
 
-  addGames() {
-    this.gameList.push(this.gameName);
+    addGames() {
+      this.gameList.push(this.gameName);
+    }
   }
-}
